3fc09d2fdc6db38c5c534e48bbe83b27119a3a97
oyatomo <oyatomo.dev@gmail.com>
2024-05-17 21:26:08 +0900

    update: added chmod

--- update.sh
+++ update.sh
@@ -7,3 +7,9 @@ rm -rf ~/.zshrc
 cp -r ./nvim ~/.config/
 cp -r ./wezterm ~/.config/
 cp ./.zshrc ~/
+
+echo "Replaced dotfiles."
+
+chmod +x ~/.config/wezterm/alias/ide.sh
+
+echo "Updated successfully."
