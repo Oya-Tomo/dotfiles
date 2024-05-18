7094fd2bf3e0d85bf7265fb4fb1aeeecc778b6ba
oyatomo <oyatomo.dev@gmail.com>
2024-05-18 19:49:31 +0900

    update: added autopairs

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -42,48 +42,4 @@ return {
             })
         end,
     },
-    {
-        "hrsh7th/nvim-cmp",
-        config = function()
-            local cmp = require('cmp')
-            local lspkind = require('lspkind')
-
-            require("cmp").setup({
-                formatting = {
-                    format = lspkind.cmp_format({
-                        mode = "symbol",
-                        maxwidth = 50,
-                        ellipsis_char = '...',
-                        before = function(entry, vim_item)
-                            return vim_item
-                        end
-                    })
-                },
-                sources = {
-                    { name = "nvim_lsp" },
-                    { name = "buffer" },
-                    { name = "path" },
-                    { name = "cmdline" },
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<Up>"] = cmp.mapping.select_prev_item(),
-                    ["<Down>"] = cmp.mapping.select_next_item(),
-                    ["<C-k>"] = cmp.mapping.select_prev_item(),
-                    ["<C-j>"] = cmp.mapping.select_next_item(),
-                    ['<C-l>'] = cmp.mapping.complete(),
-                    ['<C-e>'] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm { select = true },
-                }),
-                experimental = {
-                    ghost_text = false,
-                },
-            })
-        end,
-    },
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    -- coding support
-    "windwp/nvim-autopairs",
 }
