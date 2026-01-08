f159d5861945d4c8c9f7cd99666c3beba2302f1b
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2026-01-08 23:38:46 +0900

    update: add transparent toggle shortcut

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -25,6 +25,16 @@ wezterm.on("ide", function(window, pane)
     )
 end)
 
+wezterm.on("transparent", function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    if overrides.window_background_opacity == 0.9 then
+        overrides.window_background_opacity = 1.0
+    else
+        overrides.window_background_opacity = 0.9
+    end
+    window:set_config_overrides(overrides)
+end)
+
 return {
     leader = { key = 'l', mods = 'CTRL', timeout_milliseconds = 1000 },
     keys = {
@@ -57,10 +67,12 @@ return {
         { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard"), },
         { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard"), },
         -- Alias Shortcut
-        { key = "i", mods = "CTRL|ALT",   action = wezterm.action.EmitEvent("ide"), }
+        { key = "i", mods = "CTRL|ALT",   action = wezterm.action.EmitEvent("ide"), },
+        { key = "t", mods = "CTRL",   action = wezterm.action.EmitEvent("transparent"), }
     },
     color_scheme = "Darkside",
     font = wezterm.font_with_fallback({ "Hack Nerd Font Mono" }),
     font_size = 11,
     -- use_ime = true,
+    window_background_opacity = 0.9,
 }
