fbb371d379f0ae03841c1eb696b1faf5482cdf20
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2026-05-15 11:55:52 +0900

    update: formatted document

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,61 +2,61 @@ local wezterm = require("wezterm")
 
 -- Alias Operation Custom Event
 wezterm.on("ide", function(window, pane)
-    -- Split top/bottom; bottom pane gets further divided
-    window:perform_action(
-        wezterm.action.SplitPane({
-            direction = 'Down',
-            size = { Percent = 30 },
-        }),
-        pane
-    )
-    -- Divide the bottom pane into 3 columns
-    window:perform_action(
-        wezterm.action.SplitPane({ direction = 'Right', size = { Percent = 66 } }),
-        pane
-    )
-    window:perform_action(
-        wezterm.action.SplitPane({ direction = 'Right', size = { Percent = 50 } }),
-        pane
-    )
+  -- Split top/bottom; bottom pane gets further divided
+  window:perform_action(
+    wezterm.action.SplitPane({
+      direction = 'Down',
+      size = { Percent = 30 },
+    }),
+    pane
+  )
+  -- Divide the bottom pane into 3 columns
+  window:perform_action(
+    wezterm.action.SplitPane({ direction = 'Right', size = { Percent = 66 } }),
+    pane
+  )
+  window:perform_action(
+    wezterm.action.SplitPane({ direction = 'Right', size = { Percent = 50 } }),
+    pane
+  )
 end)
 
 return {
-    leader = { key = 'l', mods = 'CTRL', timeout_milliseconds = 1000 },
-    keys = {
-        -- Tab Operation Shortcut
-        { key = "t", mods = "ALT",       action = wezterm.action.SpawnTab("CurrentPaneDomain"), },
-        { key = "q", mods = "ALT",       action = wezterm.action.CloseCurrentTab({ confirm = true }), },
-        { key = "b", mods = "ALT",       action = wezterm.action.ActivateTabRelative( -1), },
-        { key = "n", mods = "ALT",       action = wezterm.action.ActivateTabRelative(1), },
-        { key = "b", mods = "ALT|SHIFT", action = wezterm.action.MoveTabRelative( -1), },
-        { key = "n", mods = "ALT|SHIFT", action = wezterm.action.MoveTabRelative(1), },
-        -- Pane Operation Shortcut
-        {
-            key = 'h',
-            mods = 'CTRL',
-            action = wezterm.action.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
-        },
-        {
-            key = 'v',
-            mods = 'CTRL',
-            action = wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' }),
-        },
-        { key = "q", mods = "CTRL",       action = wezterm.action.CloseCurrentPane({ confirm = true }), },
-        { key = "n", mods = "CTRL",       action = wezterm.action.ActivatePaneDirection('Next'), },
-        { key = "b", mods = "CTRL",       action = wezterm.action.ActivatePaneDirection("Prev"), },
-        { key = "h", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 1 }), },
-        { key = "l", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 1 }), },
-        { key = "k", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 1 }), },
-        { key = "j", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 1 }), },
-        -- Clipboard Operation Shortcut
-        { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard"), },
-        { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard"), },
-        -- Alias Shortcut
-        { key = "i", mods = "CTRL|ALT",   action = wezterm.action.EmitEvent("ide"), },
+  leader = { key = 'l', mods = 'CTRL', timeout_milliseconds = 1000 },
+  keys = {
+    -- Tab Operation Shortcut
+    { key = "t", mods = "ALT",       action = wezterm.action.SpawnTab("CurrentPaneDomain"), },
+    { key = "q", mods = "ALT",       action = wezterm.action.CloseCurrentTab({ confirm = true }), },
+    { key = "b", mods = "ALT",       action = wezterm.action.ActivateTabRelative(-1), },
+    { key = "n", mods = "ALT",       action = wezterm.action.ActivateTabRelative(1), },
+    { key = "b", mods = "ALT|SHIFT", action = wezterm.action.MoveTabRelative(-1), },
+    { key = "n", mods = "ALT|SHIFT", action = wezterm.action.MoveTabRelative(1), },
+    -- Pane Operation Shortcut
+    {
+      key = 'h',
+      mods = 'CTRL',
+      action = wezterm.action.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
     },
-    color_scheme = "Kanagawa (Gogh)",
-    font = wezterm.font_with_fallback({ "Hack Nerd Font Mono" }),
-    font_size = 11,
-    -- use_ime = true,
+    {
+      key = 'v',
+      mods = 'CTRL',
+      action = wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' }),
+    },
+    { key = "q", mods = "CTRL",       action = wezterm.action.CloseCurrentPane({ confirm = true }), },
+    { key = "n", mods = "CTRL",       action = wezterm.action.ActivatePaneDirection('Next'), },
+    { key = "b", mods = "CTRL",       action = wezterm.action.ActivatePaneDirection("Prev"), },
+    { key = "h", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 1 }), },
+    { key = "l", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 1 }), },
+    { key = "k", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 1 }), },
+    { key = "j", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 1 }), },
+    -- Clipboard Operation Shortcut
+    { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard"), },
+    { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard"), },
+    -- Alias Shortcut
+    { key = "i", mods = "CTRL|ALT",   action = wezterm.action.EmitEvent("ide"), },
+  },
+  color_scheme = "Kanagawa (Gogh)",
+  font = wezterm.font_with_fallback({ "Hack Nerd Font Mono" }),
+  font_size = 11,
+  -- use_ime = true,
 }
