a631f589f8f6257a721c1676831c9ea0e7db92e0
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2026-05-20 18:50:57 +0900

    update: removed unnecessary split

--- wezterm/alias/ide.sh
+++ wezterm/alias/ide.sh
@@ -5,4 +5,3 @@ wezterm cli split-pane --left --percent 30 && clear
 wezterm cli split-pane --left --percent 50 && clear
 
 wezterm cli activate-pane-direction Up && clear
-wezterm cli split-pane --right --percent 50 && clear
